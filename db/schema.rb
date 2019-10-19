e83d7b177ae71cc98209b995f6fd8f2bd766c93b
EC2 Default User <18a2028g@gmail.com>
2019-10-19 05:55:34 +0000

    approval追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191010004939) do
+ActiveRecord::Schema.define(version: 20191019055501) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20191010004939) do
     t.boolean "next_day", default: false
     t.string "outline"
     t.string "supporter"
+    t.string "approval"
   end
 
   create_table "bases", force: :cascade do |t|
