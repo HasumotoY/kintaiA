b68ebbd4b76e3193b679262a04871df4d0c7d646
EC2 Default User <18a2028g@gmail.com>
2020-01-16 05:10:48 +0000

    work_log_search_will_start

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
     @attendance = user.attendances.joins(worked_on: @first_day..@last_day).order(:worked_on)
     end
     @approval_numbers = Attendance.where(instructor: @user,approval: nil,worked_on: @first_day).count
-    @one_month_numbers = Attendance.where(one_month_instructor: @user,one_month_approval: nil).coun
+    @one_month_numbers = Attendance.where(one_month_instructor: @user,one_month_approval: nil).count
     @overtime_numbers = Attendance.where(overtime_instructor: @user, overtime_approval: nil).count
     @worked_sum = @attendances.where.not(started_at: nil).count
   end
