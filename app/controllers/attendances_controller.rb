bf24acb2436db4d265c23aff411fe6e5654cc5b5
Y.Hasumoto <18a2028g@gmail.com>
2020-04-16 00:30:50 +0900

    local-test1

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,13 +1,13 @@
 class AttendancesController < ApplicationController
 include AttendancesHelper
-  
+
   before_action :set_user,only: [:edit_one_month]
   before_action :logged_in_user, only: [:update,:edit_one_month]
   before_action :set_one_month,only: [:edit_one_month]
   before_action :admin_or_correct_user, only: [:update,:edit_one_month]
-  
+
   UPDATE_ERROR_MSG = "登録に失敗しました。やり直してください。"
-  
+
   def update
     @user = User.find(params[:user_id])
     @attendance = Attendance.find(params[:id])
@@ -19,17 +19,17 @@ include AttendancesHelper
         end
     elsif @attendance.started_at.present? &&  @attendance.finished_at.nil?
         if @attendance.update_attributes(finished_at: Time.current.change(sec: 0))
-          flash[:info] = "お疲れ様でした。"  
+          flash[:info] = "お疲れ様でした。"
         else
           flash[:danger]  =UPDATE_ERROR_MSG
         end
     end
     redirect_to @user
   end
-  
+
   def  edit_one_month
   end
-  
+
   def update_one_month
     if attendances_invalid?
       ActiveRecord::Base.transaction do
@@ -40,7 +40,7 @@ include AttendancesHelper
       end
       flash[:success] = "勤怠を更新しました。"
       redirect_to user_url(date: params[:date])
-      
+
     else
       flash[:danger] = "無効なデータがあったため、更新をキャンセルしました。"
       redirect_to attendances_edit_one_month_user_url(date: params[:date])
@@ -49,13 +49,13 @@ include AttendancesHelper
       flash[:danger] = "無効なデータがあったため、更新をキャンセルしました。"
       redirect_to attendances_edit_one_month_user_url(date: params[:date])
   end
-  
-  #残業申請機能      
+
+  #残業申請機能
   def edit_overtime
     @user = User.find(params[:user_id])
-    @attendance = @user.attendances.find(params[:id]) 
+    @attendance = @user.attendances.find(params[:id])
   end
-  
+
   def update_overtime
     @user = User.find(params[:user_id])
     @attendance = @user.attendances.find(params[:id])
@@ -71,7 +71,7 @@ include AttendancesHelper
       end
     redirect_to @user
   end
-  
+
   #所属長申請
   def update_approval
     @user = User.find(params[:user_id])
@@ -86,13 +86,13 @@ include AttendancesHelper
     end
     redirect_to @user
   end
-  
+
   #所属長承認
   def notice_approval
     @user = User.find(params[:user_id])
     @attendance = @user.attendances.find(params[:id])
   end
-  
+
   def update_notice_approval
     @user = User.find(params[:user_id])
     @attendance = @user.attendances.find(params[:id])
@@ -104,7 +104,7 @@ include AttendancesHelper
       end
     redirect_to user_url(id: @attendance.instructor.to_i)
   end
-  
+
   #勤怠変更申請
   def update_one_month_approval
     @user = User.find(params[:user_id])
@@ -121,13 +121,13 @@ include AttendancesHelper
       end
     redirect_to @user
   end
-  
+
   #勤怠変更承認
   def notice_one_month
     @user = User.find(params[:user_id])
     @attendance = @user.attendances.find(params[:id])
   end
-  
+
   def update_notice_one_month
     @user = User.find(params[:user_id])
     @attendance = @user.attendances.find(params[:id])
@@ -139,13 +139,13 @@ include AttendancesHelper
       end
       redirect_to user_url(id: @attendance.one_month_instructor.to_i)
   end
-  
-  #残業承認申請  
+
+  #残業承認申請
   def notice_overtime
     @user = User.find(params[:user_id])
     @attendance = @user.attendances.find(params[:id])
   end
-  
+
   def update_notice_overtime
     @user = User.find(params[:user_id])
     @attendance = @user.attendances.find(params[:id])
@@ -157,12 +157,12 @@ include AttendancesHelper
       end
       redirect_to user_url(id: @attendance.overtime_instructor.to_i)
   end
-  
+
   #勤怠申請ログ
   def work_log
     @user = User.find(params[:user_id])
     if params[:work_year] && params[:work_month]
-      @attendance = @user.attendances.where(worked_on: "#{params[:work_year]}-#{params[:work_month]}".in_time_zone.all_day)
+      @attendance = @user.attendances.where(worked_on: "#{params[:work_year]}",worked_on: "#{params[:work_month]}")
     elsif params[:work_year]
       @attendance = @user.attendances.where(worked_on: params[:work_year])
     elsif params[:work_month]
@@ -173,28 +173,28 @@ include AttendancesHelper
     @year = (Date.current.year - 5)..Date.current.year
     @month = Date.current.change(month: 1).month..Date.current.change(month: 12).month
   end
-  
+
   private
     def attendances_params
       params.require(:user).permit(attendances: [:started_at,:finished_at,:note,:one_month_instructor,:tomorrow])[:attendances]
     end
-    
+
     def one_month_params
       params.require(:attendance).permit(:one_month_instructor)
     end
-    
+
     def approval_params
       params.require(:attendance).permit(:instructor)
     end
-    
+
     def notice_approval_params
       params.require(:attendance).permit(:approval,:change)
     end
-    
+
     def overtime_params
       params.require(:attendance).permit(:end_estimated_time,:overtime_tomorrow,:outline,:overtime_instructor)
     end
-    
+
     def one_month_notice_params
       params.require(:attendance).permit(:one_month_approval,:one_month_change)
     end
@@ -202,11 +202,11 @@ include AttendancesHelper
     def notice_one_month_params
       params.require(:attendance).permit(:one_month_approval,:one_month_change)
     end
-    
+
     def notice_overtime_params
       params.require(:attendance).permit(:overtime_approval,:overtime_change)
     end
-    
+
     def search_params
       params.require(:q).permit(:sorts)
     end
